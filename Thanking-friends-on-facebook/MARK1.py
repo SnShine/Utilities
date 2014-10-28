995d4712e905845c73e4dc5abd226c375a26bc25
SnShine <surya123shine@gmail.com>
2014-10-28 16:34:13 +0530

    add comments

--- Thanking-friends-on-facebook/MARK1.py
+++ Thanking-friends-on-facebook/MARK1.py
@@ -1,10 +1,18 @@
+'''
+
+'''
+
+
 import facebook
 
+#getting access token from facebook. It has a lifespan of 1 hour
 graph = facebook.GraphAPI('Access token you got from facebook developers page')
+#fetching your timeline posts. limit upto 200 latest posts
 feed = graph.get_connections("me", "feed?limit=200")
+#for every post on your timeline
 for a in range (0,200):
-    post = feed["data"][a]
-    fromDetails = (post["from"])
-    fromName = fromDetails["name"]
-    graph.put_like(post["id"])
-    graph.put_object(post["id"], "comments", message="Thank you :d" + fromName)
+    post = feed["data"][a]    
+    fromDetails = (post["from"])              
+    fromName = fromDetails["name"]           #name of friend who has posted
+    graph.put_like(post["id"])				 #like that poist
+    graph.put_object(post["id"], "comments", message="Thank you :d" + fromName)     #comment on that post
