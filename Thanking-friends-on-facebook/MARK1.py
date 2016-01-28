99289c01ff98e3bca124069c9a83ab3834530777
SnShine <shine123surya@gmail.com>
2016-01-28 11:48:29 +0530

    dumping all previous changes

--- Thanking-friends-on-facebook/MARK1.py
+++ Thanking-friends-on-facebook/MARK1.py
@@ -1,5 +1,6 @@
 '''
-
+By Surya Teja Cheedella
+BITS Pilani, Hyderabad Campus
 '''
 
 
@@ -11,8 +12,8 @@ graph = facebook.GraphAPI('Access token you got from facebook developers page')
 feed = graph.get_connections("me", "feed?limit=200")
 #for every post on your timeline
 for a in range (0,200):
-    post = feed["data"][a]    
-    fromDetails = (post["from"])              
+    post = feed["data"][a]
+    fromDetails = (post["from"])
     fromName = fromDetails["name"]           #name of friend who has posted
     graph.put_like(post["id"])				 #like that poist
     graph.put_object(post["id"], "comments", message="Thank you :d" + fromName)     #comment on that post
